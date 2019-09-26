8423fac24569a6ac8e8d4567d095d15bb66e9832
Анна Бутковская-Крюкова <butovanna19@gmail.com>
2019-09-26 20:43:10 +0300

    no message

--- src/Recipes/Recipes.tsx
+++ src/Recipes/Recipes.tsx
@@ -24,12 +24,18 @@ function Recipe(props: IRecipeProps) {
     }
     return (
         <div className={classes.join(' ')} onClick={props.onSelect}>
-            <div className="listItemTitle"> {props.recipe.name}</div>
-            <img src={props.recipe.img} className="listItemImg"/>
-            <div className="listItemDescription"> {props.recipe.recipyDesctiption}</div>
-            <Button variant="contained" color="primary">
-                Edit
-            </Button>
+            <div className="container clearfix wholeField">
+                <div className="listItemTitle"> {props.recipe.name}</div>
+                <div className="mainArticle">
+                    <div className="recipeImage">
+                        <img src={props.recipe.img} className="listItemImg"/>
+                    </div>
+                    <div className="listItemDescription"> {props.recipe.recipyDesctiption}</div>
+                </div>
+                <Button variant="contained" color="primary">
+                    Edit
+                </Button>
+            </div>
         </div>
     )
 }
@@ -43,25 +49,25 @@ class Recipes extends React.Component {
         recipes: [{
             id: 1,
             isSelected: false,
-            name: "Borch",
+            name: "Borscht",
             price: 10,
             img: "https://gotovim-doma.ru/images/recipe/0/0a/00ad9d3b1ff86a92ce5935bc47a1bdee_l.jpg",
-            recipyDesctiption: "Borch description"
+            recipyDesctiption: "Borscht is a traditional Ukrainian beet and cabbage soup which is very popular in many Central and Eastern European cuisines. Variations of this dish can be different not only from country to country, but also from one Ukrainian region to another. It is also said that every Ukrainian family has original borscht recipe of its own."
         }, {
             id: 2,
             isSelected: false,
-            name: "Fish",
+            name: "Varenyky",
             price: 20,
-            img: "https://miro.medium.com/max/1899/1*mJ11PM4ZSBF0ZIAj5TebIw.png",
-            recipyDesctiption: "Fish description"
+            img: "https://chefspencil.com/wp-content/uploads/4-22.jpg",
+            recipyDesctiption: "The preparation process for varenyky or pirohi is time-consuming. However, this is probably the most unpretentious recipe in terms of filling. If you get the chance to visit a Ukrainian village or a food festival in the city, you will definitely have an opportunity to try out varenyky with the most incredible fillings – from the basic cottage cheese, mashed potatoes or sauerkraut to the more unique like olives, pumpkin, nettle or strawberries."
         },
             {
                 id: 3,
                 isSelected: false,
-                name: "Fish",
+                name: "Holubtsi",
                 price: 20,
-                img: "https://miro.medium.com/max/1899/1*mJ11PM4ZSBF0ZIAj5TebIw.png",
-                recipyDesctiption: "Fish description"
+                img: "https://panukraine.eu/uploads/abouts/shutterstock_558849211-800x533-88542.jpg",
+                recipyDesctiption: "According to the classic recipe, holubtsi is cooked from boiled cabbage, where you take every other leaf to make a new roll filled with boiled rice and meat. A variation of the dish is to use boiled vine leaves for the rolls. The rice can also be substituted with the other cereals, mushrooms, Korean carrots, etc. Classic holubtsi can be found in many restaurants in Ukraine and on banquet menus too."
             }]
     }
 
@@ -100,7 +106,7 @@ class Recipes extends React.Component {
      renderControls() {
         let text = this.state.searching ? 'cancel' : 'search'
         return(
-            <div>
+            <div className="container clearfix searchField">
                 <input
                     value={this.state.filter}
                     type="text"
